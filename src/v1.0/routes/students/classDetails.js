8b53757df800dde85aa296ce42a80029dd085ed5
Siraj-karuvathil <125243110+Siraj-karuvathil@users.noreply.github.com>
2023-03-01 15:00:38 +0530

    get c;ass by course id

--- src/v1.0/routes/students/classDetails.js
+++ src/v1.0/routes/students/classDetails.js
@@ -8,4 +8,10 @@ router.get("/", makeCallback(classController.viewClasss));
 // GET  : Get specified class
 router.get("/:id", makeCallback(classController.viewClass));
 
+// GET  : Get specified class by courseId
+router.get(
+  "/:courseId/class",
+  makeCallback(classController.viewClassByCourseId)
+);
+
 module.exports = router;
